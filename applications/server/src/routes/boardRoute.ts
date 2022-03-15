eda55a4786e73897ab8a31f9d5b73f6006028348
Sherman Wong <swonghome47@gmail.com>
2022-03-14 19:51:50 -0700

    updated boardRoute.ts

--- applications/server/src/routes/boardRoute.ts
+++ applications/server/src/routes/boardRoute.ts
@@ -2,7 +2,24 @@ import express from "express";
 import { getRepository } from "typeorm";
 import { Board } from "../models/Board";
 
-const boardRoute = express.Router();
-boardRoute.get("url here", (req, res) => {});
+const boardRouter = express.Router();
 
-export { boardRoute };
+boardRouter.get("/get-all-debug", async(req, res) => {
+    const query = await getRepository(Board).createQueryBuilder("board")
+    .select(["board.name"])
+    .getMany()
+
+    return res.status(200).json(query);
+});
+
+boardRouter.get("/search/byDescription/:name",async(req,res)=>{
+    const name = req.params.name;
+    const query = await getRepository(Board).createQueryBuilder("board")
+    .select(["board.name"])
+    .where("board.name like :name", { name:`%${name}%` })
+    .getMany()
+
+    return res.status(200).json(query);
+})
+
+export { boardRouter };
