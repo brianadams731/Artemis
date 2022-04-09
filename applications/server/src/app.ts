7f00b503d3e8b27ade3bd254ad53098ffea16510
Joseph Kois <josephkois@gmail.com>
2022-04-09 15:08:21 -0700

    added log in authentication with express-sessions

--- applications/server/src/app.ts
+++ applications/server/src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import session from "express-session";
 import { createConnection } from "typeorm";
 
 import connectionConfig from "./utils/connectionConfig";
+import { sessionConfig } from "./utils/sessionConfig";
 
 import { ticketRoute } from "./routes/ticketRoute";
 import { workspaceRoute } from "./routes/workspaceRoute";
 import { boardRouter } from "./routes/boardRoute";
+
 
 dotenv.config();
 createConnection(connectionConfig);
@@ -17,6 +20,8 @@ app.use(express.json());
 app.use("/ticket", ticketRoute);
 app.use("/workspace", workspaceRoute);
 app.use("/board", boardRouter);
+
+app.use(session(sessionConfig));
 
 app.get("/", (req, res) => {
     return res.send("Test from server");
---
+++ applications/server/src/utils/sessionConfig.ts
@@ -0,0 +1,25 @@
+import { SessionOptions, MemoryStore } from "express-session";
+import dotenv from "dotenv";
+import { v4 as uuidv4 } from 'uuid';
+dotenv.config();
+
+// USED TO ADD FIELDS TO SESSION OBJECT ie req.session.userID!
+/*declare module "express-session" {
+    interface Session {
+        userID: number;
+    }
+*/
+
+const sessionConfig: SessionOptions  = {
+    name: "userSession",
+    secret: process.env.COOKIE_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    store: new MemoryStore,
+    genid: function(req){
+        return uuidv4();
+    },
+}
+
+
+export {sessionConfig};
