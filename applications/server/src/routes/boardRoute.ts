1c6f6f0bd5c965e15b95f42b8afdd20a8985c45e
Joseph Kois <josephkois@gmail.com>
2022-04-12 20:08:08 -0700

    added update and delete to board, has issue on line 46

--- applications/server/src/routes/boardRoute.ts
+++ applications/server/src/routes/boardRoute.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { getRepository } from "typeorm";
+import { requireWithUserAsync } from "../middleware/requireWithUserAsync";
 import { Board } from "../models/Board";
 
 const boardRouter = express.Router();
@@ -14,7 +15,7 @@ boardRouter.get("/search/byName/:name",async(req,res)=>{
     return res.status(200).json(query);
 })
 
-boardRouter.post("/add", async(req,res)=>{
+boardRouter.post("/add", async (req, res)=>{
     interface BoardRequest{
         name: string;
     }
@@ -25,12 +26,43 @@ boardRouter.post("/add", async(req,res)=>{
     return res.status(201).json(board);
 })
 
-boardRouter.get("/get-all-debug", async(req, res) => {
+boardRouter.get("/get-all-debug", async (req, res) => {
     const query = await getRepository(Board).createQueryBuilder("board")
     .leftJoinAndSelect("board.tickets","ticket")
     .getMany();
 
     return res.status(200).json(query);
+});
+
+
+boardRouter.put("/:boardId", requireWithUserAsync, async (req, res) => {
+    const boardId = req.params.boardId;
+
+    if (!boardId) {
+        return res.status(500).send("Error: Board id invalid");
+    }
+    
+    const board = await Board.findOne(boardId);
+    board.name = req.body.name;
+    await board?.save();
+
+    return res.status(200).send("Board updated");
+});
+
+
+boardRouter.delete("/:boardId", requireWithUserAsync, async (req, res) => {
+    // TODO: Test route
+    const boardId = req.params.boardId;
+    if(!boardId) {
+        return res.status(500).send("Error: Board id invalid");
+    }
+    const board = await Board.findOne(boardId);
+
+    const didDelete = await board?.remove();
+    if(!didDelete){
+        res.status(500).send("Error: Board failed to remove");
+    }
+    return res.status(200).send("Board removed");
 });
 
 export { boardRouter };
