a1ae55560e7fce3bbf6a8637df17b262b228f65b
brianadams731 <brianadams731@gmail.com>
2022-04-15 13:32:39 -0700

    Chore: Changed landing page to workspace debug

--- applications/client/artemis/src/pages/Milestone2.tsx
+++ applications/client/artemis/src/pages/Milestone2.tsx
@@ -1,59 +1,29 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getEndpoint } from "../utils/apiEndpoints";
-// import m2 from "../styles/Milestone2CSS";
 
 const Milestone2 = (): JSX.Element => {
-    interface Ticket {
-        id: number;
-        description: string;
-        comment: string;
-        openDate: string;
+    interface WorkSpaceDebug {
+        id: string
     }
-    interface Board {
-        name: string;
-        tickets: Ticket[];
-    }
-    const [board, setBoard] = useState<Board>();
-    const [searchBox, setSearchBox] = useState<string>("");
+    const [debug, setDebug] = useState<WorkSpaceDebug[]>();
     useEffect(() => {
-        (async () => {
-            const res = await fetch(getEndpoint("board_get_all_debug")!)
-            const parsedRes = await res.json();
-            setBoard(parsedRes);
-        })()
-    }, [])
-
-    if (!board) {
-        return <div>Loading...</div>
+        (async function () {
+            const rawReq = await fetch(getEndpoint("workspace_debug")!);
+            const parsedReq = await rawReq.json();
+            setDebug(parsedReq)
+        }());
+    })
+    if (!debug) {
+        return <h1>Loading Debug Menu...</h1>
     }
-
     return (
         <main>
-            <Link  to="dashboard/workspace/4b786df1-9fd3-4f17-a6d6-b339756fb562">Click to View Work Space Example</Link>
-
-            <form onSubmit={(e) => {
-                e.preventDefault();
-                searchBox !== "" ?
-                    (async () => {
-                        const res = await fetch(`${getEndpoint("board_search_by_name")}${searchBox}`)
-                        const parsedRes = await res.json();
-                        setBoard(parsedRes);
-                    })() :
-                    (async () => {
-                        const res = await fetch(getEndpoint("board_get_all_debug")!)
-                        const parsedRes = await res.json();
-                        setBoard(parsedRes);
-                    })();
-            }}>
-                <input  type="text" value={searchBox} onChange={(e) => {
-                    setSearchBox(e.target.value);
-                }} />
-                <button type="submit">Search</button>
-            </form>
-            <pre>
-                {JSON.stringify(board, null, 2)}
-            </pre>
+            {debug.map((item) => {
+                return (
+                        <Link style={{display:"block"}} to={`dashboard/workspace/${item.id}`}>Click to View Workspace ID {item.id}</Link>
+                )
+            })}
         </main>
     )
 }
