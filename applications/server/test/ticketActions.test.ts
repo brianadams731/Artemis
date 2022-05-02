d2d4f4aadfd8231dd3ea901939915c0f4c100e47
Joseph Kois <josephkois@gmail.com>
2022-05-01 18:13:31 -0700

    prog on tests

--- applications/server/test/ticketActions.test.ts
+++ applications/server/test/ticketActions.test.ts
@@ -9,9 +9,23 @@ interface Request {
 let ticketId: any;
 
 describe('Ticket Functionality', () => {
+    test('GET /search/byDescription/:description', (done) => {
+        request(app)
+        .get("/search/byDescription/:description")
+        .expect(200)
+        .expect((res: { body: { comment: string; description: string; }; }) => {
+            res.body.comment = 'test';
+            res.body.description = 'test';
+        })
+        .end((err:any, res:any) => {
+            if (err) return done(err);
+            return done();
+        });
+    });
+    
     test('POST /add/byBoardId/:boardId', (done) => {
         request(app)
-        .post('/add')
+        .post('/add/byBoardId/:boardId')
         .send({ 
             comment: 'test', 
             description: 'test',
@@ -30,7 +44,7 @@ describe('Ticket Functionality', () => {
 
     test('PUT /byId/:ticketId/:priorityTicket',  (done) => {
         request(app)
-        .put(`/byId/${ticketId}`)
+        .put('/byId/:ticketId/:priorityTicket')
         .send({ 
             comment: 'test2',
             description: 'test2',
@@ -40,7 +54,7 @@ describe('Ticket Functionality', () => {
             res.body.comment = "test2";
             res.body.description = "test2";
         })
-        .end((err: any, res: any) => {
+        .end((err:any, res:any) => {
             if (err) return done(err);
             return done();
         });
@@ -48,7 +62,7 @@ describe('Ticket Functionality', () => {
 
     test('DELETE /byId/:ticketId', (done) => {
         request(app)
-        .put(`/byId/${ticketId}`)
+        .put('/byId/:ticketId')
         .send({ 
             comment: 'test2',
             description: 'test2',
@@ -58,7 +72,7 @@ describe('Ticket Functionality', () => {
             res.body.comment = "test2";
             res.body.description = "test2";
         })
-        .end((err: any, res: any) => {
+        .end((err:any, res:any) => {
             if (err) return done(err);
             return done();
         });
