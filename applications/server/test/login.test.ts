ae03f61d3375480a0da0e7b6c7fd4d037402225f
brianadams731 <brianadams731@gmail.com>
2022-05-01 20:14:03 -0700

    Added: Login test

--- applications/server/test/login.test.ts
+++ applications/server/test/login.test.ts
@@ -6,22 +6,32 @@ import connectionConfig from "../src/utils/connectionConfig";
 import dotenv from "dotenv";
 
 describe('Ticket Functionality', () => {
+    let cookies: any;
     let connection: Connection;
+
     beforeAll(async ()=>{
         dotenv.config();
         connection = await createConnection(connectionConfig);
+
+        const res = await request(app).post("/login").send({
+            email:"test@test.com",
+            password:"test"
+        });
+        cookies = res.header['set-cookie'][0];
     })
+
     afterAll(async ()=>{
         await connection.close();
     })
 
     test('Login', async () => {
-        const res = await request(app).post("/login").send({
+        let res = await request(app).post("/login").send({
             email:"test@test.com",
             password:"test"
         });
         expect(res.status).toEqual(200);
 
-        
+        res = await request(app).get("/login/test").set("Cookie",[`${cookies}`]).send();
+        expect(res.status).toEqual(200);
     });
 });
