f62d023abd4c7b8cadbf36224be22c2966a95460
brianadams731 <brianadams731@gmail.com>
2022-05-13 21:26:19 -0700

    Fixed: Render issue with profile modal

--- applications/client/artemis/src/components/ProfileModal.tsx
+++ applications/client/artemis/src/components/ProfileModal.tsx
@@ -1,29 +1,21 @@
-import { useNavigate } from 'react-router-dom';
-import { IProfile } from '../interfaces/IProfile';
-import styles from '../styles/WorkspaceTile.module.scss';
+import styles from "../styles/ProfileModal.module.scss";
 
-interface Base{
-    closeModal: ()=>void;
+interface Props {
+    closeModal: () => void;
 }
 
-interface Props extends Base{ 
-    // id: string;
-    state: "open"
-    // username: string;
-    // email: string;
-    // organization: string;
-    // profile: (profile: IProfile)=>void;
-}
-
-const ProfileModal = ({closeModal}:Props): JSX.Element => {
-    const navigate = useNavigate();
+const ProfileModal = ({ closeModal }: Props): JSX.Element => {
     return (
         <div className={styles.wrapper} onClick={()=>{
-            navigate("/dashboard/workspace/");
+            closeModal();
         }}>
-            <h3>Username</h3>
-            <h3>Email</h3>
-            <h3>Organization</h3>
+            <div className={styles.modalWrapper} onClick={(e)=>{
+                e.stopPropagation();
+            }}>
+                <h3>Username</h3>
+                <h3>Email</h3>
+                <h3>Organization</h3>
+            </div>
         </div>
     )
 }
